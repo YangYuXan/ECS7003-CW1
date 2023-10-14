f3c2c01442374db85b7705e95eb9d1e6eb07b874
YangYu <amdyuy@163.com>
2023-10-14 01:17:03 +0100

    Add Camera Function
    1.Free move
    2.Rotate
    3.Fouc to character

--- Assets/Game/C_Camera.cs
+++ Assets/Game/C_Camera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 using Camera = UnityEngine.Camera;
 
@@ -9,38 +10,84 @@ public class C_Camera : MonoBehaviour
     public enum CameraMode
     {
         freedom,
-        track
+        track,
+        clockwiseRotate, 
+        anticlockwiseRotation
     }
 
-    public Vector3 moveValue;
+    public GameObject targetPointMark;
+
+    public Vector3 moveValue = new Vector3(0,0,0);
     public float _moveSpeed;
-    public int currentIndex;
     public CameraMode cameraMode = CameraMode.freedom;
-    private Camera camera;
+    private bool _moveCamera=false;
+
+    float _duration = 10f;
+    float _timer = 0f;
+    private Vector3 moveDir;
+
+
     Vector3 hitpoint = Vector3.zero;
+    RaycastHit casHit;
 
     // Start is called before the first frame update
     void Start()
     {
-        camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += moveValue;
+
+        transform.position += moveDir;
+
+        switch (cameraMode)
+        {
+            case CameraMode.clockwiseRotate:
+                if (casHit.collider.gameObject!=null)
+                {
+                    transform.RotateAround(casHit.point, casHit.collider.gameObject.transform.up, 3);
+                }
+                break;
+
+            case CameraMode.anticlockwiseRotation:
+                if (casHit.collider.gameObject != null)
+                {
+                    transform.RotateAround(casHit.point, casHit.collider.gameObject.transform.up, -3);
+                }
+                break;
+
+            case CameraMode.track:
+                if (_timer < _duration - 9.3)
+                { 
+                    float t = _timer / _duration;
+                    transform.position = Vector3.Lerp(transform.position,
+                        new Vector3(casHit.collider.gameObject.transform.position.x, transform.position.y, casHit.collider.gameObject.transform.position.y), t);
+                    _timer += Time.deltaTime;
+                }
+                else if (Vector3.Distance(transform.position, casHit.collider.gameObject.transform.position) < 20)
+                {
+                    cameraMode = CameraMode.freedom;
+                    _timer = 0;
+                }
+                break;
+            case CameraMode.freedom:
+                break;
+        }
+
     }
 
     void OnCameraMove(InputValue value)
     {
-        moveValue = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y)*_moveSpeed;
-
+        moveValue.z = value.Get<Vector2>().y;
+        moveValue.x = value.Get<Vector2>().x;
+        moveDir = new Vector3(transform.forward.x, 0, transform.forward.z) * moveValue.z + transform.right * moveValue.x;
     }
 
     void OnInteract()
     {
         var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        RaycastHit casHit;
+        
 
         Debug.DrawRay(transform.position, ray.direction*200, Color.red);
 
@@ -48,16 +95,55 @@ public class C_Camera : MonoBehaviour
         {
             if (casHit.collider.gameObject.tag == "Character")
             {
-                print("jit");
+                //SetMoveCamera(true);
+                cameraMode = CameraMode.track;
+            }
+
+            if (casHit.collider.gameObject.tag == "Terrain")
+            {
+                Transform spawnPoint = casHit.collider.gameObject.transform;
+                Instantiate(targetPointMark, casHit.point, spawnPoint.rotation);
             }
             
         }
     }
 
-    /*
-    float GetMovespeed()
+    void OnCameraRotate(InputValue value)
     {
-        return _moveSpeed;
+        if (value.Get<Vector2>().y!=0)
+        {
+            print("rotate");
+            var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (value.Get<Vector2>().y > 0)
+            {
+                Physics.Raycast(ray, out casHit);
+                cameraMode = CameraMode.clockwiseRotate;
+            }
+            else
+            {
+                Physics.Raycast(ray, out casHit);
+                cameraMode = CameraMode.anticlockwiseRotation;
+            }
+            
+        }
+        else
+        {
+            print("stop");
+            cameraMode = CameraMode.freedom;
+        }
     }
-    */
+
+    void OnZoom(InputValue value)
+    {
+        //float height = transform.position.y-;
+
+
+        print("value: "+value.Get<Vector2>());
+    }
+
+    bool GetMoveCameraValue()
+    {
+        return _moveCamera;
+    }
+
 }
